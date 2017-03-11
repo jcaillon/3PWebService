fdf25dba4e7599530b34a62ce6f2f7497a39384f
Julien Caillon <julien.caillon@gmail.com>
2017-03-11 21:51:03 +0100

    user by country

--- v1.6.4/index.php
+++ v1.6.4/index.php
@@ -99,8 +99,8 @@ switch ($action) {
     // ------------------------------
     case 'getPing':
 
-        $req = SPDO::getInstance()->prepare('SELECT UUID, userName, firstPing, lastPing, location, version, nbPing FROM MySoft_ping ORDER BY lastPing DESC');
-        $req->execute();
+        $req = SPDO::getInstance()->prepare('SELECT UUID, userName, firstPing, lastPing, location, version, nbPing FROM MySoft_ping WHERE softName = :softName ORDER BY lastPing DESC');
+        $req->execute(array('softName' => $softName));
 
         $users = array();
         while ($user = $req->fetch()) {
@@ -117,42 +117,6 @@ switch ($action) {
 
         header('Content-type: application/json');
         echo json_encode($users, JSON_PRETTY_PRINT);
-        break;
-
-
-    // ------------------------------
-    // Get ping
-    // ------------------------------
-    case 'getRecap':
-
-        $recap = array();
-
-        $req = SPDO::getInstance()->prepare('SELECT COUNT(1) AS nbUsers FROM MySoft_ping');
-        $req->execute();
-        $res = $req->fetch();
-        $recap['totalUsers'] = $res['nbUsers'];
-
-        $req = SPDO::getInstance()->prepare('SELECT COUNT(1) AS nbUsers FROM MySoft_ping WHERE DATE(lastPing) BETWEEN DATE_SUB(DATE(NOW()), INTERVAL 1 DAY) AND CURDATE()');
-        $req->execute();
-        $res = $req->fetch();
-        $recap['dayUsers'] = $res['nbUsers'];
-
-        $req = SPDO::getInstance()->prepare('SELECT COUNT(1) AS nbUsers FROM MySoft_ping WHERE DATE(lastPing) BETWEEN DATE_SUB(DATE(NOW()), INTERVAL 6 DAY) AND CURDATE()');
-        $req->execute();
-        $res = $req->fetch();
-        $recap['weeklyUsers'] = $res['nbUsers'];
-
-        $req = SPDO::getInstance()->prepare('SELECT version, COUNT(*) AS num FROM MySoft_ping GROUP BY version');
-        $req->execute();
-        while ($res = $req->fetch()) {
-            $recap['usersByVersion'][] = array(
-                'version'=>$res['version'],
-                'nbUsers'=>$res['num']
-            );
-        }
-
-        header('Content-type: application/json');
-        echo json_encode($recap, JSON_PRETTY_PRINT);
         break;
 
 
@@ -224,8 +188,8 @@ switch ($action) {
     // ------------------------------
     case 'getBugs':
 
-        $req = SPDO::getInstance()->prepare('SELECT originVersion, originMethod, originLine, receptionTime, nbReceived, UUID, message, fullException FROM MySoft_bugs ORDER BY receptionTime DESC');
-        $req->execute();
+        $req = SPDO::getInstance()->prepare('SELECT originVersion, originMethod, originLine, receptionTime, nbReceived, UUID, message, fullException FROM MySoft_bugs WHERE softName = :softName ORDER BY receptionTime DESC');
+        $req->execute(array('softName' => $softName));
 
         $users = array();
         while ($user = $req->fetch()) {
@@ -244,6 +208,60 @@ switch ($action) {
         header('Content-type: application/json');
         echo json_encode($users, JSON_PRETTY_PRINT);
         break;
+
+
+
+    // ------------------------------
+    // Get Recap
+    // ------------------------------
+    case 'getRecap':
+
+        $recap = array();
+
+        $req = SPDO::getInstance()->prepare('SELECT location FROM MySoft_ping WHERE softName = :softName');
+        $req->execute(array('softName' => $softName));
+        $user = $req->fetch();
+
+
+        $req = SPDO::getInstance()->prepare('SELECT COUNT(1) AS nbUsers FROM MySoft_ping WHERE softName = :softName');
+        $req->execute(array('softName' => $softName));
+        $res = $req->fetch();
+        $recap['totalUsers'] = $res['nbUsers'];
+
+        $req = SPDO::getInstance()->prepare('SELECT COUNT(1) AS nbUsers FROM MySoft_ping WHERE softName = :softName AND DATE(lastPing) BETWEEN DATE_SUB(DATE(NOW()), INTERVAL 1 DAY) AND CURDATE()');
+        $req->execute(array('softName' => $softName));
+        $res = $req->fetch();
+        $recap['dayUsers'] = $res['nbUsers'];
+
+        $req = SPDO::getInstance()->prepare('SELECT COUNT(1) AS nbUsers FROM MySoft_ping WHERE softName = :softName AND DATE(lastPing) BETWEEN DATE_SUB(DATE(NOW()), INTERVAL 6 DAY) AND CURDATE()');
+        $req->execute(array('softName' => $softName));
+        $res = $req->fetch();
+        $recap['weeklyUsers'] = $res['nbUsers'];
+
+        $req = SPDO::getInstance()->prepare('SELECT version, COUNT(*) AS nbUsers FROM MySoft_ping WHERE softName = :softName GROUP BY version');
+        $req->execute(array('softName' => $softName));
+        $recap['usersByVersion'] = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        $req = SPDO::getInstance()->prepare('SELECT SUBSTR(location, 1, 2) AS country, COUNT(*) AS nbUsers FROM MySoft_ping WHERE softName = :softName AND concat("", SUBSTR(location, 1, 1) * 1) <> SUBSTR(location, 1, 1) GROUP BY SUBSTR(location, 1, 2) ORDER BY nbusers DESC');
+        $req->execute(array('softName' => $softName));
+        $usersByCountry = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        $countryCode = array(
+            "AF"=>"Afghanistan", "AX"=>"Aland Islands", "AL"=>"Albania", "DZ"=>"Algeria", "AS"=>"American Samoa", "AD"=>"Andorra", "AO"=>"Angola", "AI"=>"Anguilla", "AQ"=>"Antarctica", "AG"=>"Antigua and Barbuda", "AR"=>"Argentina", "AM"=>"Armenia", "AW"=>"Aruba", "AU"=>"Australia", "AT"=>"Austria", "AZ"=>"Azerbaijan", "BS"=>"Bahamas", "BH"=>"Bahrain", "BD"=>"Bangladesh", "BB"=>"Barbados", "BY"=>"Belarus", "BE"=>"Belgium", "BZ"=>"Belize", "BJ"=>"Benin", "BM"=>"Bermuda", "BT"=>"Bhutan", "BO"=>"Bolivia", "BA"=>"Bosnia and Herzegovina", "BW"=>"Botswana", "BV"=>"Bouvet Island", "BR"=>"Brazil", "VG"=>"British Virgin Islands", "IO"=>"British Indian Ocean Territory", "BN"=>"Brunei Darussalam", "BG"=>"Bulgaria", "BF"=>"Burkina Faso", "BI"=>"Burundi", "KH"=>"Cambodia", "CM"=>"Cameroon", "CA"=>"Canada", "CV"=>"Cape Verde", "KY"=>"Cayman Islands", "CF"=>"Central African Republic", "TD"=>"Chad", "CL"=>"Chile", "CN"=>"China", "HK"=>"Hong Kong, SAR China", "MO"=>"Macao, SAR China", "CX"=>"Christmas Island", "CC"=>"Cocos (Keeling) Islands", "CO"=>"Colombia", "KM"=>"Comoros", "CG"=>"Congo (Brazzaville)", "CD"=>"Congo, (Kinshasa)", "CK"=>"Cook Islands", "CR"=>"Costa Rica", "CI"=>"Côte d'Ivoire", "HR"=>"Croatia", "CU"=>"Cuba", "CY"=>"Cyprus", "CZ"=>"Czech Republic", "DK"=>"Denmark", "DJ"=>"Djibouti", "DM"=>"Dominica", "DO"=>"Dominican Republic", "EC"=>"Ecuador", "EG"=>"Egypt", "SV"=>"El Salvador", "GQ"=>"Equatorial Guinea", "ER"=>"Eritrea", "EE"=>"Estonia", "ET"=>"Ethiopia", "FK"=>"Falkland Islands (Malvinas)", "FO"=>"Faroe Islands", "FJ"=>"Fiji", "FI"=>"Finland", "FR"=>"France", "GF"=>"French Guiana", "PF"=>"French Polynesia", "TF"=>"French Southern Territories", "GA"=>"Gabon", "GM"=>"Gambia", "GE"=>"Georgia", "DE"=>"Germany", "GH"=>"Ghana", "GI"=>"Gibraltar", "GR"=>"Greece", "GL"=>"Greenland", "GD"=>"Grenada", "GP"=>"Guadeloupe", "GU"=>"Guam", "GT"=>"Guatemala", "GG"=>"Guernsey", "GN"=>"Guinea", "GW"=>"Guinea-Bissau", "GY"=>"Guyana", "HT"=>"Haiti", "HM"=>"Heard and Mcdonald Islands", "VA"=>"Holy See (Vatican City State)", "HN"=>"Honduras", "HU"=>"Hungary", "IS"=>"Iceland", "IN"=>"India", "ID"=>"Indonesia", "IR"=>"Iran, Islamic Republic of", "IQ"=>"Iraq", "IE"=>"Ireland", "IM"=>"Isle of Man", "IL"=>"Israel", "IT"=>"Italy", "JM"=>"Jamaica", "JP"=>"Japan", "JE"=>"Jersey", "JO"=>"Jordan", "KZ"=>"Kazakhstan", "KE"=>"Kenya", "KI"=>"Kiribati", "KP"=>"Korea (North)", "KR"=>"Korea (South)", "KW"=>"Kuwait", "KG"=>"Kyrgyzstan", "LA"=>"Lao PDR", "LV"=>"Latvia", "LB"=>"Lebanon", "LS"=>"Lesotho", "LR"=>"Liberia", "LY"=>"Libya", "LI"=>"Liechtenstein", "LT"=>"Lithuania", "LU"=>"Luxembourg", "MK"=>"Macedonia, Republic of", "MG"=>"Madagascar", "MW"=>"Malawi", "MY"=>"Malaysia", "MV"=>"Maldives", "ML"=>"Mali", "MT"=>"Malta", "MH"=>"Marshall Islands", "MQ"=>"Martinique", "MR"=>"Mauritania", "MU"=>"Mauritius", "YT"=>"Mayotte", "MX"=>"Mexico", "FM"=>"Micronesia, Federated States of", "MD"=>"Moldova", "MC"=>"Monaco", "MN"=>"Mongolia", "ME"=>"Montenegro", "MS"=>"Montserrat", "MA"=>"Morocco", "MZ"=>"Mozambique", "MM"=>"Myanmar", "NA"=>"Namibia", "NR"=>"Nauru", "NP"=>"Nepal", "NL"=>"Netherlands", "AN"=>"Netherlands Antilles", "NC"=>"New Caledonia", "NZ"=>"New Zealand", "NI"=>"Nicaragua", "NE"=>"Niger", "NG"=>"Nigeria", "NU"=>"Niue", "NF"=>"Norfolk Island", "MP"=>"Northern Mariana Islands", "NO"=>"Norway", "OM"=>"Oman", "PK"=>"Pakistan", "PW"=>"Palau", "PS"=>"Palestinian Territory", "PA"=>"Panama", "PG"=>"Papua New Guinea", "PY"=>"Paraguay", "PE"=>"Peru", "PH"=>"Philippines", "PN"=>"Pitcairn", "PL"=>"Poland", "PT"=>"Portugal", "PR"=>"Puerto Rico", "QA"=>"Qatar", "RE"=>"Réunion", "RO"=>"Romania", "RU"=>"Russian Federation", "RW"=>"Rwanda", "BL"=>"Saint-Barthélemy", "SH"=>"Saint Helena", "KN"=>"Saint Kitts and Nevis", "LC"=>"Saint Lucia", "MF"=>"Saint-Martin (French part)", "PM"=>"Saint Pierre and Miquelon", "VC"=>"Saint Vincent and Grenadines", "WS"=>"Samoa", "SM"=>"San Marino", "ST"=>"Sao Tome and Principe", "SA"=>"Saudi Arabia", "SN"=>"Senegal", "RS"=>"Serbia", "SC"=>"Seychelles", "SL"=>"Sierra Leone", "SG"=>"Singapore", "SK"=>"Slovakia", "SI"=>"Slovenia", "SB"=>"Solomon Islands", "SO"=>"Somalia", "ZA"=>"South Africa", "GS"=>"South Georgia and the South Sandwich Islands", "SS"=>"South Sudan", "ES"=>"Spain", "LK"=>"Sri Lanka", "SD"=>"Sudan", "SR"=>"Suriname", "SJ"=>"Svalbard and Jan Mayen Islands", "SZ"=>"Swaziland", "SE"=>"Sweden", "CH"=>"Switzerland", "SY"=>"Syrian Arab Republic (Syria)", "TW"=>"Taiwan, Republic of China", "TJ"=>"Tajikistan", "TZ"=>"Tanzania, United Republic of", "TH"=>"Thailand", "TL"=>"Timor-Leste", "TG"=>"Togo", "TK"=>"Tokelau", "TO"=>"Tonga", "TT"=>"Trinidad and Tobago", "TN"=>"Tunisia", "TR"=>"Turkey", "TM"=>"Turkmenistan", "TC"=>"Turks and Caicos Islands", "TV"=>"Tuvalu", "UG"=>"Uganda", "UA"=>"Ukraine", "AE"=>"United Arab Emirates", "GB"=>"United Kingdom", "US"=>"United States of America", "UM"=>"US Minor Outlying Islands", "UY"=>"Uruguay", "UZ"=>"Uzbekistan", "VU"=>"Vanuatu", "VE"=>"Venezuela (Bolivarian Republic)", "VN"=>"Viet Nam", "VI"=>"Virgin Islands, US", "WF"=>"Wallis and Futuna Islands", "EH"=>"Western Sahara", "YE"=>"Yemen", "ZM"=>"Zambia", "ZW"=>"Zimbabwe"
+        );
+
+        foreach($usersByCountry as $val) {
+            $key = $val["country"];
+            if (isset($countryCode[$val["country"]])) {
+                $key = $countryCode[$val["country"]];
+            }
+            $recap["usersByCountry"][] = array("country"=>$key , "nbUsers"=>$val["nbUsers"]);
+        }
+
+        header('Content-type: application/json');
+        echo json_encode($recap, JSON_PRETTY_PRINT);
+        break;
+
 
     default:
         rollbackAndDie();
